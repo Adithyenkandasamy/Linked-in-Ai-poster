01b30543616c0c6d0784edb1c89cc2926b0bca11
Adithyenkandasamy <adithyen1@gmail.com>
2025-06-27 17:57:29 +0530

    new gemini update

--- gemini_ai.py
+++ gemini_ai.py
@@ -5,10 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 def generate_post(topic: str) -> str:
     prompt = f"Write a professional LinkedIn post on the topic: {topic}"
     response = model.generate_content(prompt)
     return response.text.strip()
-    
